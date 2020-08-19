1f693bb07f894fcead8500836950f731d4889780
denis <e1527687@student.tuwien.ac.at>
2020-08-19 11:39:53 +0200

    list comp and generators aha tut5 is done

--- list_comprehension_and_generator_expressions.py
+++ list_comprehension_and_generator_expressions.py
@@ -13,7 +13,48 @@ print(xyz2)
 xyz = (i for i in range(5))  # generator
 print('printing generator')
 print(xyz)
+########################################################################################################################
+input_list = [5, 6, 2, 10, 15, 20, 5, 2, 1, 3]
 
 
+def div_by_five(num):
+    if num % 5 == 0:
+        return True
+    else:
+        return False
 
+
+xyz = (i for i in input_list if div_by_five(i))
+# 27 and 29-32 are doing the same thing (beside list vs generator)
+# xyz = []
+# for i in input_list:
+#     if div_by_five(i):
+#         xyz.append(i)
+
+for i in xyz:
+    print(i)
+# 34 and 37 are doing the same thing
+[print(i) for i in xyz]
+
+########################################################################################################################
+
+[[print(i, ii) for ii in range(5)] for i in range(5)]
+# 41 and 43-45 doing the same thing
+for i in range(
+        5):  # third thing to write in 41 ---> always doing stuff backwards if I want to write "oneliner for loop" aka 41
+    for ii in range(5):  # second thing to write in 41
+        print(i, ii)  # first thing to write in 41
+
+xyz = [[(i, ii) for ii in range(5)] for i in range(5)]  # list of tuples
+print(xyz)
+
+xyz = [[[i, ii] for ii in range(5)] for i in range(5)]  # list of lists
+print(xyz)
+
+xyz = ([[i, ii] for ii in range(5)] for i in range(5))  # generator expression over which one I can iterate
+print(xyz)
+
+print([i for i in xyz])  # iterating over my generator
+# with big list you will run out of memory
+# with big generators you will run out of time
 
